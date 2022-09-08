3584c6fbb134aaf9849629778b949ca56a3d690a
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-08 13:20:28 -0300

    criando tables com nodejs

--- js/1_drop.js
+++ js/1_drop.js
@@ -1,11 +1,11 @@
 const db = require('./_database')
 
 async function dropTables(){//função para deletar tabelas
-    await db.connect()
-    await db.query(`DROP TABLE evento CASCADE`)
+    await db.connect()//conecta com o banco
+    await db.query(`DROP TABLE evento CASCADE`)//comandos do banco de dados
     await db.query(`DROP TABLE participante CASCADE`)
     await db.query(`DROP TABLE evento_participante CASCADE`)
-    await db.end()
+    await db.end()//encerra conexão
 
     console.log('Tabelas removidas');
 }
