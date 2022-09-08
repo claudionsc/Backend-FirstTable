950ec1f52c5167d61ced97c03c6a54aa82ed92da
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-08 12:24:56 -0300

    add _database e primeiro arquivo para exclusão de tabelas existentes

--- js/_database.js
+++ js/_database.js
@@ -1,1 +1,11 @@
 const pg = require('pg') //require importa um módulo
+
+const client = new pg.Client({
+    user: 'postgresql',
+    host: 'localhost',
+    database: 'postgresql',
+    password: 'postgresql',
+    port: 5432,
+})
+
+module.exports = client
