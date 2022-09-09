ee5958fbed43b92be1a655e896d9c6c5c18ae6e2
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-09 08:20:54 -0300

    consulta relacional

--- js/4_select.js
+++ js/4_select.js
@@ -12,7 +12,11 @@ async function listData(){
     console.log('PARTICIPANTES')
     console.log(result.rows);
 
-   
+   result = await db.query(
+    'SELECT e.nome AS evento, p.nome AS participante FROM evento AS e, participante AS p, evento_participante ep WHERE ep.evento_id = e.id AND ep.participante_id = p.id'
+   )
+   console.log('EVENTO COM PARTICIPANTES')
+   console.log(result.rows);
 
 }
 
