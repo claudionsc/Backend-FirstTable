a6e870e7a1f61c0be94cad60effa708a38290a63
Claudio Nascimento <claudio.alves20112011@hotmail.com>
2022-09-10 11:41:15 -0300

    Iniciando com sequelize

--- js/1_drop.js
+++ js/1_drop.js
@@ -3,7 +3,7 @@ const db = require('./_database')
 async function dropTables(){//função para deletar tabelas
     await db.connect()//conecta com o banco
     await db.query(`DROP TABLE evento CASCADE`)//comandos do banco de dados
-    await db.query(`DROP TABLE participante CASCADE`)
+    await db.query(`DROP TABLE participantes CASCADE`)
     await db.query(`DROP TABLE evento_participante CASCADE`)
     await db.end()//encerra conexão
 
